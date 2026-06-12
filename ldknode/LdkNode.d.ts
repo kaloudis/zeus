3b7ba234146f31aecfaf2dd0ee9206d860539ab3
Evan Kaloudis <zeusln@tutanota.com>
2026-06-12 13:38:19 -0400

    deps: update ldk-node to v0.7.0-zeus-bimodal

--- ldknode/LdkNode.d.ts
+++ ldknode/LdkNode.d.ts
@@ -454,6 +454,7 @@ export interface ILdkNodeModule {
     ): Promise<void>;
 
     setVssBuildTimeout(timeoutSeconds: number): Promise<void>;
+    setPaymentRetryTimeout(timeoutSeconds: number): Promise<void>;
 
     // Crypto Methods
     mnemonicToSeed(
